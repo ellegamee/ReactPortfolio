de9af03e892d20728dac14900e338031186085af
Elias Axonov <ed.axonov@gmail.com>
2022-04-10 04:26:17 +0200

    header initiallized styling with flex and links to other subdomains

--- portfolio/src/Components/Header.jsx
+++ portfolio/src/Components/Header.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
+    const StyledHeader = {
+        'display': 'flex',
+    }
+
+    const StyledMenu = {
+        'display': 'flex',
+        'margin-left': 'auto',
+    }
+
+    const StyleLinks = {
+        'text-decoration': 'none',
+        'color': 'black',
+        'margin': '15px',
+    }
+
     return(
-        <h4>Elias Axonov (Header)</h4>
+        <div style={StyledHeader}>
+            <Link to='/' style={StyleLinks}>Elias Axonov</Link>
+            
+            <div style={StyledMenu}>
+                <Link to='/archive' style={StyleLinks}>Archive</Link>
+                <Link to='/about' style={StyleLinks}>About</Link>
+            </div>
+        </div>
     )
 }
