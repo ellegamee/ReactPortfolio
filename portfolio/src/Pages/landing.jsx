371e0ccffd9abf58de284e176e71f8beef2b8ec1
Elias Axonov <ed.axonov@gmail.com>
2022-04-12 16:15:54 +0200

    new background image with waves and temp text placed with some styling. This welcomes the users into a welcoming place with my main colors that i representent.

--- portfolio/src/Pages/landing.jsx
+++ portfolio/src/Pages/landing.jsx
@@ -1,5 +1,21 @@
+import backgroundImage from '../Images/BackgroundWave.svg'
+
 export default function Landing() {
-    return(
-        <p>This is the front page</p>
+    const wrapper_div = {
+        'background-image': `url(${backgroundImage})`,
+        'position': 'absolute',
+        
+        'top': '0',
+        'bottom': '0',
+        'left': '0',
+        'right': '0',
+    }
+    
+    // <img style={styled_background} src= alt='' />
+    return (
+        <div style={wrapper_div}>
+            <p>My Portfolio</p>
+            <p>Where adventure leads</p>
+        </div>
     )
 }
