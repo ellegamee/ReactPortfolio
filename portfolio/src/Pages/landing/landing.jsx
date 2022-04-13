00a24fcb568f2b6571a587d6e0d091870a6e48fc
Elias Axonov <ed.axonov@gmail.com>
2022-04-13 16:50:21 +0200

    Landing page finalization
    Landing page will now move on. Layout and text is responsive on different devices but google nest and samsung fold.
    
    Portfolio text is transforming with animation when hovering for a nice ux to see its a link etc.

--- portfolio/src/Pages/landing/landing.jsx
+++ portfolio/src/Pages/landing/landing.jsx
@@ -1,12 +1,16 @@
 import React from "react"
+import { Link } from 'react-router-dom'
 import "./landing.css"
 
 export default function Landing() {
+    const intro_text = "Hello!"
+    const name_text = "Portfolio"
+
     return (
         <div className="landing__background">            
             <div className="text__wrapper">
-                <p className="text__wrapper--intro">This is</p>
-                <p className="text__wrapper--name">Elias</p>
+                <p className="text__wrapper--intro">{intro_text}</p>
+                <Link to="/home" className="text__wrapper--name">{name_text}</Link>
             </div>
         </div>
     )
