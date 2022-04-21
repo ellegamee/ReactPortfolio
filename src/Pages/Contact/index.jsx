5c075eb31ba69478c0949128058d9c0d4f2d7dd3
Elias Axonov <ed.axonov@gmail.com>
2022-04-21 15:07:11 +0200

    getform.io forms
    Use of getforms.io to get information from forms and get it to one place and make it work without any own backend.

--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -4,11 +4,11 @@ import "./index.css"
 export default function archive() {
     return(
         <React.Fragment>
-            <form className="form">
+            <form className="form" action="https://getform.io/f/fe132cd1-39ba-4da6-ba07-f0c4523f0190" method="POST">
                 <label className="contact_intro">Contact me</label>
-                <input type="text" className="form__input" placeholder="Full Name" />
-                <input type="text" className="form__input" placeholder="Email" />
-                <input type="text" className="form__input" placeholder="Message" />
+                <input type="text" className="form__input" name="name" placeholder="Full Name" />
+                <input type="text" className="form__input" name="email" placeholder="Email" />
+                <input type="text" className="form__input" name="message" placeholder="Message" />
                 <input type="submit" className="form__submit" value="Send" />
             </form>
         </React.Fragment>
