58b4c8b2018db990759cf6572af5923441131867
Elias Axonov <ed.axonov@gmail.com>
2022-05-03 10:32:37 +0200

    Construction removal
    Removal of "under construction" for empty slate to make a home page as I want it.

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,8 +4,6 @@ import "./index.css"
 export default function Home() {
     return (
         <div className="wrapper">
-            <p className="info__text">Website is under construction</p>
-            <div className="loader"></div>
         </div>
     )
 }
