4caf594281887d8135b814ac427589f91772d681
Elias Axonov <ed.axonov@gmail.com>
2022-05-10 09:17:03 +0200

    Simple first step slider
    Main page slider created with swiperjs. First step into the real stuff with making a nice cool main page.

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import "./index.css"
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import "swiper/css";
+import 'swiper/css/navigation'
+
+import { Navigation, Keyboard } from 'swiper';
+
 export default function Home() {
     return (
-        <div className="wrapper">
+        <div>
+            <Swiper
+                style={{ width: '100%', height: '100vh' }}
+                modules={[Navigation, Keyboard]}
+                slidesPerView={4}
+                centeredSlides={true}
+                keyboard={{
+                    enable: true, 
+                    onlyInViewport: false
+                }}
+                navigation={true}
+            >
+                <SwiperSlide className='swiper-slide'>Slide 1</SwiperSlide>
+                <SwiperSlide className='swiper-slide'>Slide 2</SwiperSlide>
+                <SwiperSlide className='swiper-slide'>Slide 3</SwiperSlide>
+            </Swiper>
         </div>
     )
 }
