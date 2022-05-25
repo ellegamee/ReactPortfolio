35c3657a55734c555993ec13ace8ceb89636b86a
Elias Axonov <ed.axonov@gmail.com>
2022-05-25 10:58:21 +0200

    Burger menu onClick classes
    When onClick event runs on burger menu the <hr> tags rotates and forms a X. This will then pan out into a menu which will be closed by the X and turn back into a burger.

--- src/Layout/Header/index.jsx
+++ src/Layout/Header/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { GrHomeRounded } from 'react-icons/gr'
+import { HiOutlineMenuAlt3 } from 'react-icons/hi'
 import './index.css'
 
 export default function Header() {
+    const [cross1, setCross1] = useState("")
+    const [cross2, setCross2] = useState("")
+    const [cross3, setCross3] = useState("")
+
+    function toggle_burger_menu() {
+        console.log("menu clicked")
+
+        if (cross1 === "active_cross1") {
+            setCross1("")
+            setCross2("")
+            setCross3("")
+
+        } else {
+            setCross1("active_cross1")
+            setCross2("active_cross2")
+            setCross3("active_cross3")
+        }
+    }
+
     return(
         <div className="header">
             <Link to='/home' className="links"><GrHomeRounded /></Link>
@@ -11,6 +31,12 @@ export default function Header() {
             <div className="menu">
                 {/*<Link to='/archive' className="links">About</Link>*/}
                 <Link to='/contact' className="links">Contact</Link>
+            </div>
+
+            <div className="burger_menu" onClick={toggle_burger_menu}>
+                <hr className={cross1}></hr>
+                <hr className={cross2}></hr>
+                <hr className={cross3}></hr>
             </div>
         </div>
     )
