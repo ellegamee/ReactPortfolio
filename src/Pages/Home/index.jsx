7dfdc9898fd5385dbf3bcc6ed3126c578e3bf811
Elias Axonov <ed.axonov@gmail.com>
2022-05-25 09:47:04 +0200

    h2 aboslute solved
    Absolute position solved with relative position on parent div which solves the problem. Result is a h2 tag which works good with mobile.

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -38,7 +38,7 @@ export default function Home() {
                 <SwiperSlide className='swiper-slide'>
                     <img src={sudoku} alt="" className="projekt1" style={{height: "100%", maxWidth: "800px", minWidth: "600px"}}/>
                     <div style={{color: 'white'}}>
-                        <h2 style={{top: "26%", left: "-45%"}}>Gymnasiearbete</h2>
+                        <h2>Gymnasiearbete</h2>
                         <a 
                         className="github_link" 
                         href="https://github.com/ellegamee/GymnasieArbete-2021" 
@@ -55,7 +55,7 @@ export default function Home() {
                 <SwiperSlide className='swiper-slide'>
                     <img src={taskmanager} alt="" style={{height: "60%", left: "25%", top: "20%"}}/>
                     <div>
-                        <h2 style={{top: "20%", left: "-35%"}}>Taskmanager</h2>
+                        <h2>Taskmanager</h2>
                         <a 
                         className="github_link" 
                         href="https://github.com/ellegamee/taskmanager" 
@@ -71,7 +71,7 @@ export default function Home() {
                 <SwiperSlide className='swiper-slide' style={{position: "relative", overflow: "hidden"}}>
                     <img src={hangman} alt="" style={{left: "10%", top: "30%", width: "480px"}}/>
                     <div>
-                        <h2 style={{top: "11%", left: "-20%"}}>Hangman</h2>
+                        <h2>Hangman</h2>
                         <a 
                         className="github_link" 
                         href="https://github.com/ellegamee/GymnasieArbete-2021" 
@@ -87,7 +87,7 @@ export default function Home() {
                 <SwiperSlide className='swiper-slide' style={{position: "relative", overflow: "hidden"}}>
                     <img src={twitchbot} alt="" className="projekt4" style={{top: "4%", width: "1200px"}}/>                
                     <div>
-                        <h2 style={{top: "15%", left: "-25%"}}>Twitch Bot</h2>
+                        <h2>Twitch Bot</h2>
                         <a 
                         className="github_link" 
                         href="https://github.com/ellegamee/twitch_bot" 
